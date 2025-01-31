19a959eb41acd268178025429438447f0974717c
mtszd <mtszd.ia@gmail.com>
2025-01-31 15:21:34 -0300

    Resend test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
+import vercel from '@astrojs/vercel/serverless';
 import svelte from "@astrojs/svelte";
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://sunvey.com',
-  integrations: [mdx(), sitemap(), svelte()]
+  integrations: [mdx(), sitemap(), svelte()],
+  output: "server",
+  adapter: vercel()
 });
